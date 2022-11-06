bd94072d43be55eb0da8d307f99980eb52ff59b3
per lejon <per.lejon@netigate.se>
2022-11-06 16:05:36 +0100

    python tests

--- setup.py
+++ setup.py
@@ -1,25 +1,75 @@
-from setuptools import setup, find_packages
-import codecs
+# from setuptools import setup, find_packages
+# import codecs
+# import os
+#
+# here = os.path.abspath(os.path.dirname(__file__))
+#
+# with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+#     long_description = "\n" + fh.read()
+#
+# VERSION = "0.0.1"
+# DESCRIPTION = "Custom type for pydantic"
+#
+# setup(
+#     name="pydantic-custom-types",
+#     version=VERSION,
+#     description=DESCRIPTION,
+#     long_description_content_type="text/markdown",
+#     long_description=long_description,
+#     author="Per Lejon",
+#     author_email="<per.lejon@netigate.se>",
+#     url="https://github.com/netigate/pydantic-custom-types",
+#     packages=find_packages(exclude="tests*"),
+#     # install_requires=[],
+# )
+
+
+import setuptools
+import subprocess
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
+remote_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+if "-" in remote_version:
+    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
+    # pip has gotten strict with version numbers
+    # so change it to: "1.3.3+22.git.gdf81228"
+    # See: https://peps.python.org/pep-0440/#local-version-segments
+    v,i,s = remote_version.split("-")
+    remote_version = v + "+" + i + ".git." + s
 
-VERSION = "0.0.1"
-DESCRIPTION = "Custom type for pydantic"
+assert "-" not in remote_version
+assert "." in remote_version
 
-setup(
+assert os.path.isfile("pydantic_custom_types/version.py")
+with open("pydantic_custom_types/VERSION", "w", encoding="utf-8") as fh:
+    fh.write("%s\n" % remote_version)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="pydantic-custom-types",
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=long_description,
+    version=remote_version,
     author="Per Lejon",
-    author_email="<per.lejon@netigate.se>",
+    author_email="per.lejon@netigate.se",
+    description="Some custom pydantic types",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/netigate/pydantic-custom-types",
-    packages=find_packages(exclude="tests*"),
+    packages=setuptools.find_packages(),
+    package_data={"cf_remote": ["VERSION"]},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: The MIT License (MIT)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
     # install_requires=[],
 )
 
